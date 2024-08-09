a629bf181e3012b915ee497131534fc83e9fba1a
Doug Hatcher <superterran@gmail.com>
2024-08-09 17:56:46 +0000

    server renders the readme instead of hello world

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+const marked = require('marked');
 const app = express();
 
 const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  fs.readFile('./README.md', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading README.md');
+    } else {
+      var rawMarkup = marked.parse(data);
+
+      res.send(`${rawMarkup}`);
+    }
+  });
 });
 
 app.listen(port, () => {
