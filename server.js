e2f0fd3d2f16c39986d3c801b44e1b9249eca599
Doug Hatcher <superterran@gmail.com>
2024-08-09 16:18:43 -0400

    clean up and styling

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
 const marked = require('marked');
 const app = express();
 
 const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
 
-app.get('/', (req, res) => {
-  fs.readFile('./README.md', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error reading README.md');
-    } else {
-      var rawMarkup = marked.parse(data);
+// Function to generate HTML response
+const generateHtmlResponse = (cssData, rawMarkup) => `
+  <html>
+    <head>
+      <title>Example Node App on Adobe Commerce Cloud</title>
+      <style>${cssData}</style>
+    </head>
+    <body>
+      ${rawMarkup}
+    </body>
+  </html>
+`;
 
-      res.send(`${rawMarkup}`);
-    }
-  });
+app.get('/', async (req, res) => {
+  try {
+    // Read README.md file
+    const data = await fs.readFile('./README.md', 'utf8');
+    const rawMarkup = marked.parse(data);
+
+    // Read server.css file
+    const cssData = await fs.readFile('./server.css', 'utf8');
+
+    // Send HTML response
+    res.set('Content-Type', 'text/html');
+    res.send(generateHtmlResponse(cssData, rawMarkup));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error reading files');
+  }
 });
 
 app.listen(port, () => {
